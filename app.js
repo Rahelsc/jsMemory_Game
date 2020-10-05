a6e1daf664f32fcb582d02e1ef54046ad20cff63
Rachel Schwartz <rahelsc@gmail.com>
2020-10-05 07:56:59 +0300

    updated images. Added some of the js

---
+++ app.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //card options
+    const cardArray = [
+        {
+            name: 'autumn',
+            img: 'images/autumn.jpg'
+        },
+        {
+            name: 'bridge',
+            img: 'images/bridge.jpg'
+        },
+        {
+            name: 'cricket',
+            img: 'images/cricket.jpg'
+        },
+        {
+            name: 'elephant',
+            img: 'images/elephant.jpg'
+        },
+        {
+            name: 'flowers in cup',
+            img: 'images/flowers-in-cup.jpg'
+        },
+        {
+            name: 'lake and trees',
+            img: 'images/lake-and-trees.jpg'
+        },
+        {
+            name: 'leaves',
+            img: 'images/leaves.jpg'
+        },
+        {
+            name: 'pirple tree',
+            img: 'images/pirple-tree.jpg'
+        },
+        {
+            name: 'swing',
+            img: 'images/swing.jpg'
+        },
+    ]
+
+    const grid = document.querySelector('.grid')
+
+    //create your board
+    
+})
